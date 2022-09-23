30a8f269c8c1bae68f74aa1aa78e09c249533619
yoshihiko11 <mtd11tky02-2011@ezweb.ne.jp>
2022-09-23 17:50:59 +0900

    master.blade作成1

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Laravel</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <div class='posts'>
+            <dev class='post'>
+                <h2 class='title'>タイトル</h2>
+                <p class='body'>This is a sample body.</p>
+            </dev>
+        </div>
+    </body>
+</html>
